a0a088096e739d27d869262b70c7a94b1c716618
David Massoud <davmassoud@gmail.com>
2016-04-19 01:16:38 +0200

    changed proof validations

--- app/models/proof.rb
+++ app/models/proof.rb
@@ -1,13 +1,15 @@
 class Proof < ActiveRecord::Base
 	validates :picture, :longitude, :latitude, :taken_at, presence: true
-
+	validates_uniqueness_of [:longitude, :latitude, :taken_at]
 	mount_uploader :picture, PictureUploader
-	before_save :geolocate
+	before_validation :geolocate
 
-	def geolocate 
-		p = self.picture.path 
-		self.longitude = EXIFR::JPEG.new(picture.path).gps.longitude
-		self.latitude =  EXIFR::JPEG.new(picture.path).gps.latitude
-		self.taken_at = EXIFR::JPEG.new(picture.path).date_time_original
+	def geolocate 	
+		p = EXIFR::JPEG.new(picture.path)
+		
+		self.longitude = p.gps.longitude if !p.gps.nil?
+		self.latitude =  p.gps.latitude	if !p.gps.nil?
+		self.taken_at = p.date_time_original if !p.date_time_original.nil?
+	
 	end
 end
